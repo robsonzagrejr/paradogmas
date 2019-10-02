ad2d33c0a99014b57cc1180477e3a87de2332921
Robson Zagre Júnior <r.zagre.jr@gmail.com>
2019-10-02 13:47:42 -0300

    Checkpoint 1

--- wolkenkratzer.hs
+++ wolkenkratzer.hs
@@ -1,17 +1,17 @@
 module Wolkenkratzer (Wk,
-                      my_example) where
+                      myExample) where
 
 import Matrices
 
 type VisSkyscraper = [[Int]] -- array of visible skyscrapers top-right-bottom-left
 
-data Wk = Matrix VisSkyscraper
+type Wk = (Matrix, VisSkyscraper)
 
 myExample :: Int -> Wk
-myExample 1 = (setMatrix 3 3) ([[0,2,0],[3,2,0],[2,0,0],[1,0,2]])
+myExample 1 = ((setMatrix 3 3), ([[0,2,0],[3,2,0],[2,0,0],[1,0,2]]))
 
---    print(my_matrix 1)
+-- print(my_matrix 1)
 
- --   putStrLn $ showMatrix (my_matrix 1)
+-- putStrLn $ showMatrix (my_matrix 1)
 
-   -- putStrLn $ showMatrixPossibilities (my_matrix 1)
+-- putStrLn $ showMatrixPossibilities (my_matrix 1)
